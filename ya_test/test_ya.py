ccb53c215ceae923aee21da0bad8b95796480f17
abadmaev <https://github.com/Natasha2202/Matchtv_web>
2021-02-03 21:42:29 +0300

    Initial commit

---
+++ ya_test/test_ya.py
@@ -0,0 +1,13 @@
+import pytest
+import requests
+
+
+
+def test_check_ya(base_url):
+    response = requests.get(base_url)
+    assert response.status_code == 200
+
+@pytest.mark.parametrize("id", ["qwewtegfufngig", 23, 0, -1])
+def test_negative_yandex(base_url, id):
+    response = requests.get(f"{base_url}/{id}")
+    assert response.status_code == 404
