f35dc789830a1261c7071ee9f9252bd47ecfa098
abadmaev <https://github.com/Natasha2202/Matchtv_web>
2022-02-21 19:55:36 +0300

    add3

--- jsonplase_api/test_jsonPlase.py
+++ jsonplase_api/test_jsonPlase.py
@@ -3,11 +3,13 @@ import pytest
 POST_MAX = 100
 PHOTOS_MAX = 5000
 
+
 @pytest.mark.parametrize("post_id", [1, POST_MAX])
 def test_check_id(base_url, session, post_id):
     response = session.get(f"{base_url}/posts/{post_id}")
     assert response.status_code == 200
     assert response.json()["id"] == post_id
+
 
 @pytest.mark.parametrize("post_id", [-1, 0, "phy", POST_MAX + 1])
 def test_negative_check(base_url, session, post_id):
@@ -15,17 +17,20 @@ def test_negative_check(base_url, session, post_id):
     assert resp.status_code == 404
     assert resp.json() == {}
 
-def test_check_lengt_lest(base_url,session):
+
+def test_check_lengt_lest(base_url, session):
     res = session.get(f"{base_url}/posts")
     assert len(res.json()) == 100
+
 
 def test_photos_lengt(base_url, session):
     resp_check = session.get(f"{base_url}/photos")
     assert resp_check.status_code == 200
     assert len(resp_check.json()) == 5000
 
+
 def test_create(session, base_url):
-    payload = {"title":"footer", "body": "nsus", "userId":1}
+    payload = {"title": "footer", "body": "nsus", "userId": 1}
     response = session.post(f"{base_url}/posts", json=payload)
 
     assert response.status_code == 201
@@ -35,10 +40,8 @@ def test_create(session, base_url):
     assert json_resp["title"] == payload["title"]
     assert json_resp["body"] == payload["body"]
 
+
 def test_delete(session, base_url):
     response = session.delete(f"{base_url}/posts/1")
     assert response.status_code == 200
     assert len(response.json()) == 0
-
-
-
