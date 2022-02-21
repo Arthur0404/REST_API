620cc7255a88a314a81ddf2c609917f31a06f66d
abadmaev <https://github.com/Natasha2202/Matchtv_web>
2022-02-21 19:55:22 +0300

    add2

--- jsonplase_api/test_schema_list.py
+++ jsonplase_api/test_schema_list.py
@@ -1,13 +1,13 @@
 import json
-from jsonschema import  validate
+from jsonschema import validate
+
 
 def assert_validate_schema(data, schema_file):
     with open(schema_file) as file_json:
         schema = json.load(file_json)
         return validate(instance=data, schema=schema)
 
+
 def test_validate(base_url, session):
     response = session.get(f"{base_url}/posts/1")
     assert_validate_schema(response.json(), "../schemas/post_schema.json")
-
-
